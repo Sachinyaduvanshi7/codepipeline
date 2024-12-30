e746e3e8cac8b821d98c535da739a86593f08a65
SACHIN YADAV <132833023+Sachinyaduvanshi7@users.noreply.github.com>
2024-12-30 10:53:35 +0530

    Update after_install.sh-1

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-#_Change_Working_Directory
-cd /home/ec2-user/server
-
-#_Remove_Unused_Code
-rm -rf node_modules
-rm -rf build
-
-#Install_node_modules_&_Make_React_Build
-npm  install
-npm run build
+echo "Running AfterInstall hook..."
+sudo rm -rf /var/www/react-app/*
+sudo cp -r /var/www/react-app /usr/share/nginx/html
