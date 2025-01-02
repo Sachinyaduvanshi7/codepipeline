4ac50d040353ed2d8b05fb0443cc04dd28e172d9
SACHIN YADAV <132833023+Sachinyaduvanshi7@users.noreply.github.com>
2025-01-02 12:03:45 +0530

    Update and rename after_install.sh to afterinstall.sh

--- scripts/after_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Running AfterInstall hook..."
-sudo rm -rf /var/www/react-app/*
-sudo cp -r /var/www/react-app /usr/share/nginx/html
---
+++ scripts/afterinstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/ubuntu
+sudo cp -r /home/ubuntu/build /var/www/html
